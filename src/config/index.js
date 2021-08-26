014715fa1fa876786ea7129d792534aed9229e1c
Juan Rodriguez <sjdonado@uninorte.edu.co>
2021-08-26 08:27:43 -0500

    feat: :wrench: Sequelize + postgreSQL
    Add Dockerfile.dev, add docker-compose.yml, add development database config, remove dotenv

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,4 @@
-require('dotenv').config();
-
 module.exports = {
+  DATABASE_URL: process.env.DATABASE_URL,
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
 };
--- src/database/config/index.js
+++ src/database/config/index.js
@@ -1,6 +1,10 @@
-const { TEST_DATABASE_URL } = require('../../config');
+const { DATABASE_URL, TEST_DATABASE_URL } = require('../../config');
 
 module.exports = {
+  development: {
+    url: DATABASE_URL,
+    dialect: 'postgres',
+  },
   test: {
     url: TEST_DATABASE_URL,
     dialect: 'sqlite',
