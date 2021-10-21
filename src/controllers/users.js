d7eaf60a48eb9e1b37b50a5af98df9b4b1f3af5a
Rafael Velandia <ravelandia@uninorte.edu.co>
2021-10-21 01:52:26 +0000

    feat: Se modifco createUser y se verificaron los 3 primeros test

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -6,7 +6,6 @@ const UserSerializer = require('../serializers/UserSerializer');
 const createUser = async (req, res, next) => {
   try {
     const { body } = req;
-
     if (body.password !== body.passwordConfirmation) {
       throw new ApiError('Passwords do not match', 400);
     }
@@ -16,7 +15,13 @@ const createUser = async (req, res, next) => {
       email: body.email,
       name: body.name,
       password: body.password,
+      active: undefined,
     });
+
+    if (user.username === undefined || user.email === undefined
+      || user.name === undefined || user.password === undefined) {
+      throw new ApiError('Payload must contain name, username, email and password', 400);
+    }
 
     res.json(new UserSerializer(user));
   } catch (err) {
@@ -28,6 +33,7 @@ const getUserById = async (req, res, next) => {
   try {
     const { params } = req;
 
+    console.log(params);
     const user = await User.findOne({ where: { id: params.id } });
 
     res.json(new UserSerializer(user));
