68923282aeb49ac4a5469ea90b4987eace4ebb88
Giovanoti <cgmoreno@uninorte.edu.co>
2021-10-22 23:52:10 -0500

    Fix: errors from Linter

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -16,7 +16,7 @@ const createUser = async (req, res, next) => {
       email: body.email,
       name: body.name,
       password: body.password,
-    }
+    };
 
     if (Object.values(data).some((val) => val === undefined)) {
       throw new ApiError('Payload must contain name, username, email and password', 400);
@@ -35,28 +35,28 @@ const updateUser = async (req, res, next) => {
     const { params } = req;
 
     const userFound = await User.findOne({ where: { id: params.id } });
-    if(!userFound || !userFound.active){
+    if (!userFound || !userFound.active) {
       throw new ApiError('User not found', 400);
     }
 
-    if (Object.keys(body).map((e) =>{
-      if(e !== 'name' && e !== 'username' && e !== 'email'){
+    if (Object.keys(body).map((e) => {
+      if (e !== 'name' && e !== 'username' && e !== 'email') {
         throw new ApiError('Payload can only contain username, email or name', 400);
-      };
+      }
     }));
 
     const data = {};
     if (body.name) {
-      data['name'] = body.name;
+      data.name = body.name;
     }
     if (body.username) {
-      data['username'] = body.username;
+      data.username = body.username;
     }
     if (body.email) {
-      data['email'] = body.email;
+      data.email = body.email;
     }
 
-    const user = await User.update({ where: { id: params.id } }, data )
+    const user = await User.update({ where: { id: params.id } }, data );
     res.json(new UserSerializer(user));
   } catch (err) {
     next(err);
@@ -67,28 +67,26 @@ const deleteUser = async (req, res, next) => {
   try {
     const { params } = req;
     const userFound = await User.findOne({ where: { id: params.id } });
-    if(!userFound || !userFound.active){
+    if (!userFound || !userFound.active) {
       throw new ApiError('User not found', 400);
     }
 
-    const user = await User.update({ where: { id: params.id } }, {active: false} )
-    res.status(200).json({status: 'success', data: null});
+    const user = await User.update({ where: { id: params.id } }, { active: false } );
+    res.status(200).json({ status: 'success', data: null });
   } catch (err) {
     next(err);
   }
 };
 
-//prueba
 const getUserById = async (req, res, next) => {
   try {
     const { params } = req;
 
     const user = await User.findOne({ where: { id: params.id } });
    
-    if(!user || !user.active){
+    if (!user || !user.active) {
       throw new ApiError('User not found', 400);
     }
-
 
     res.json(new UserSerializer(user));
   } catch (err) {
@@ -100,5 +98,5 @@ module.exports = {
   createUser,
   getUserById,
   updateUser,
-  deleteUser
+  deleteUser,
 };
