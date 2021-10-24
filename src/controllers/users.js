6a202d0a3e816326a5f69dd5b9991f5815b97f29
jdlagares <sportymammal@gmail.com>
2021-10-24 16:27:13 -0500

    feat: Add endpoint desactivateUsers & modify endpoint

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,4 +1,6 @@
 const ApiError = require('../utils/ApiError');
+const BaseSerializer = require('../serializers/BaseSerializer');
+
 
 const User = require('../models/user');
 const UserSerializer = require('../serializers/UserSerializer');
@@ -11,12 +13,18 @@ const createUser = async (req, res, next) => {
       throw new ApiError('Passwords do not match', 400);
     }
 
-    const user = await User.create({
+    const userData = {
       username: body.username,
       email: body.email,
       name: body.name,
       password: body.password,
-    });
+    };
+
+    if (Object.values(userData).some((val) => val === undefined)) {
+      throw new ApiError('Payload must contain name, username, email and password', 400);
+    }
+
+    const user = await User.create(userData);
 
     res.json(new UserSerializer(user));
   } catch (err) {
@@ -24,13 +32,39 @@ const createUser = async (req, res, next) => {
   }
 };
 
+
 const getUserById = async (req, res, next) => {
   try {
     const { params } = req;
 
     const user = await User.findOne({ where: { id: params.id } });
 
+    if (!user || !user.active) {
+      throw new ApiError('User not found', 400);
+    }
+
     res.json(new UserSerializer(user));
+  } catch (err) {
+    next(err);
+  }
+};
+
+const deactivateUser = async (req, res, next) => {
+  try {
+    const { params } = req;
+
+    const user = await User.findOne({ where: { id: params.id } });
+
+    if (!user || !user.active) {
+      throw new ApiError('User not found', 400);
+    }
+
+    await User.update(
+      { where: { id: params.id } },
+      { active: false },
+    );
+
+    res.json(new BaseSerializer('success', null));
   } catch (err) {
     next(err);
   }
@@ -42,6 +76,10 @@ const updateUser = async (req, res, next) => {
     const { body } = req;
 
     const user = await User.findOne({ where: { id: params.id } });
+
+    if (!user || !user.active) {
+      throw new ApiError('User not found', 400);
+    }
 
     const userData = {
       username: body.username,
@@ -68,4 +106,5 @@ module.exports = {
   createUser,
   getUserById,
   updateUser,
+  deactivateUser,
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -10,4 +10,6 @@ router.post('/', UsersController.createUser);
 
 router.put('/:id', UsersController.updateUser);
 
+router.delete('/:id', UsersController.deactivateUser);
+
 module.exports = router;
