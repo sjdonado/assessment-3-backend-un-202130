7bf30bfd98af105768e0eeaf8f20875daf6b893a
Luis <elcontreras@uninorte.edu.co>
2021-10-24 17:12:38 -0500

    Primer envio sin cambios

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,5 +7,5 @@ const UsersController = require('../controllers/users');
 router.get('/:id', UsersController.getUserById);
 
 router.post('/', UsersController.createUser);
-
+//Primer Envio sin cambios
 module.exports = router;
