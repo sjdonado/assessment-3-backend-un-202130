fcf4e1222f53f52cdffee397527da84fc607d749
QuantumGorilla <manotasja@uninorte.edu.co>
2021-10-24 10:42:26 -0500

    feat: deleteUser and updateUser

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,41 +2,94 @@ const ApiError = require('../utils/ApiError');
 
 const User = require('../models/user');
 const UserSerializer = require('../serializers/UserSerializer');
+const BaseSerializer = require('../serializers/BaseSerializer');
 
-const createUser = async (req, res, next) => {
-  try {
-    const { body } = req;
+const createUser = async(req, res, next) => {
+    try {
+        const { body } = req;
 
-    if (body.password !== body.passwordConfirmation) {
-      throw new ApiError('Passwords do not match', 400);
+        if (Object.values(body).some((val) => val === undefined)) {
+            throw new ApiError('Payload must contain name, username, email and password', 400);
+        }
+        
+        if (body.password !== body.passwordConfirmation) {
+            throw new ApiError('Passwords do not match', 400);
+        }
+        
+        const user = await User.create({
+            username: body.username,
+            email: body.email,
+            name: body.name,
+            password: body.password,
+        });
+
+        res.json(new UserSerializer(user));
+    } catch (err) {
+        next(err);
     }
-
-    const user = await User.create({
-      username: body.username,
-      email: body.email,
-      name: body.name,
-      password: body.password,
-    });
-
-    res.json(new UserSerializer(user));
-  } catch (err) {
-    next(err);
-  }
 };
 
-const getUserById = async (req, res, next) => {
-  try {
-    const { params } = req;
+const updateUser = async(req, res, next) => {
+    try {
+        const { body } = req;
+        const { params } = req;
 
-    const user = await User.findOne({ where: { id: params.id } });
+        if (Object.keys(body).some((val) => val !== 'name' && val !== 'username' && val !== 'email')) {
+            throw new ApiError('Payload can only contain username, email or name', 400);
+        }
+        
+        let user = await User.findOne({ where: { id: params.id } });
+        if(!user || !user.active){
+            throw new ApiError('User not found', 400);
+        }
 
-    res.json(new UserSerializer(user));
-  } catch (err) {
-    next(err);
-  }
+        user = await User.update({ where: { id: user.id } }, {
+            username: body.username,
+            email: body.email,
+            name: body.name
+        });
+
+        res.json(new UserSerializer(user));
+    } catch (err) {
+        next(err);
+    }
+};
+
+const deleteUser = async(req, res, next) => {
+    try {
+        const { params } = req;
+
+        let user = await User.findOne({ where: { id: params.id } });
+        if(!user || !user.active){
+            throw new ApiError('User not found', 400);
+        }
+        user = await User.update({ where: { id: user.id } }, {
+            active: false
+        });
+
+        res.json(new BaseSerializer('success', null));
+    } catch (err) {
+        next(err);
+    }
+};
+
+const getUserById = async(req, res, next) => {
+    try {
+        const { params } = req;
+
+        const user = await User.findOne({ where: { id: params.id } });
+        if(!user || !user.active){
+            throw new ApiError('User not found', 400);
+        }
+        res.json(new UserSerializer(user));
+    } catch (err) {
+        next(err);
+    }
 };
 
 module.exports = {
-  createUser,
-  getUserById,
+    createUser,
+    getUserById,
+    deleteUser,
+    updateUser
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -5,7 +5,8 @@ const router = express.Router();
 const UsersController = require('../controllers/users');
 
 router.get('/:id', UsersController.getUserById);
-
+router.delete('/:id', UsersController.deleteUser);
+router.put('/:id', UsersController.updateUser);
 router.post('/', UsersController.createUser);
 
 module.exports = router;
