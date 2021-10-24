8e43f69e2570894ac79a7fc7e6273d73978b1a86
TheLokestraps <ivanestebanperezdlo@hotmail.com>
2021-10-24 01:20:07 -0500

    fix: Fixed verification of payloads

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -11,11 +11,17 @@ const createUser = async (req, res, next) => {
       throw new ApiError('Passwords do not match', 400);
     }
 
+    if (body.username === undefined || body.email === undefined
+      || body.name === undefined || body.password === undefined) {
+      throw new ApiError('Payload must contain name, username, email and password', 400);
+    }
+
     const user = await User.create({
       username: body.username,
       email: body.email,
       name: body.name,
       password: body.password,
+      active: undefined,
     });
 
     res.json(new UserSerializer(user));
@@ -24,13 +30,40 @@ const createUser = async (req, res, next) => {
   }
 };
 
+const updateUser = async (req, res, next) => {
+  try {
+    const { params } = req;
+    const { body } = req;
+
+    const user = await User.findOne({ id: params.id });
+
+    const updatedUser = await User.update({ where: { id: params.id } }, body);
+    res.json(new UserSerializer(updatedUser));
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deactivateUser = async (req, res, next) => {
+
+};
+
 const getUserById = async (req, res, next) => {
   try {
     const { params } = req;
 
     const user = await User.findOne({ where: { id: params.id } });
+    if (user !== undefined && user.active === true) {
+      console.log(user);
 
-    res.json(new UserSerializer(user));
+      if (user.lastLoginDate === null) {
+        user.active = undefined;
+      }
+
+      res.json(new UserSerializer(user));
+    } else {
+      throw new ApiError('User not found', 400);
+    }
   } catch (err) {
     next(err);
   }
@@ -38,5 +71,7 @@ const getUserById = async (req, res, next) => {
 
 module.exports = {
   createUser,
+  updateUser,
+  deactivateUser,
   getUserById,
 };
