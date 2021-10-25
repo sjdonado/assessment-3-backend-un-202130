199657b06b0f0b37fae87c02cebc447bfc028ecf
= <=>
2021-10-25 13:27:06 -0500

    feat: ✨ add update and deactivate user routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,4 +8,8 @@ router.get('/:id', UsersController.getUserById);
 
 router.post('/', UsersController.createUser);
 
+router.put('/:id', UsersController.updateUser);
+
+router.delete('/:id', UsersController.deactivateUser);
+
 module.exports = router;
