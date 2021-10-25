b0e1584cab9eaeae30acb7d2ef6707b44a860189
Ubuntu <azureuser@Backend-Eval2.hcqbgbqd5coupid5ynsor4hqxc.jx.internal.cloudapp.net>
2021-10-25 01:27:25 +0000

    Arreglando errores 4

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,9 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 const UsersController = require('../controllers/users');
+
 router.get('/:id', UsersController.getUserById);
+
 router.post('/', UsersController.createUser);
+
 router.delete('/:id', UsersController.deactivateUser);
+
 router.put('/:id', UsersController.updateUser);
 
 module.exports = router;
