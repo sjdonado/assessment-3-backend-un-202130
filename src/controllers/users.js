044641820380b1e9eb9327a6be7d7a0c9939ad3a
MRmario2411 <mariou@uninorte.edu.co>
2021-10-25 22:29:32 +0000

    fix: 🔧 endpoint createUser fixed and test passed

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -3,13 +3,31 @@ const ApiError = require('../utils/ApiError');
 const User = require('../models/user');
 const UserSerializer = require('../serializers/UserSerializer');
 
+function ThrowError(message, code) {
+  if (message !== '') throw new ApiError(message, code);
+}
+
+const finalError = {
+  message: '',
+  code: '',
+};
+
 const createUser = async (req, res, next) => {
+  finalError.message = '';
+  finalError.code = '';
   try {
     const { body } = req;
-
+    // const bodyKeys = Object.keys(body);
     if (body.password !== body.passwordConfirmation) {
       throw new ApiError('Passwords do not match', 400);
     }
+    if (Object.keys(body).includes('name', 'username', 'email', 'password')) {
+      // payload completed
+    } else {
+      finalError.message = 'Payload must contain name, username, email and password';
+      finalError.code = 400;
+    }
+    ThrowError(finalError.message, finalError.code);
 
     const user = await User.create({
       username: body.username,
@@ -17,6 +35,8 @@ const createUser = async (req, res, next) => {
       name: body.name,
       password: body.password,
     });
+
+    user.active = undefined;
 
     res.json(new UserSerializer(user));
   } catch (err) {
