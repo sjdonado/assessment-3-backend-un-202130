b33304033bf3fc71fa1f2082077236c537e7e131
dolceymendozajr <dolceymendozajr@gmail.com>
2021-10-26 17:11:33 +0000

    feat: ✨assesment 3 (users crud)

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -11,6 +11,13 @@ const createUser = async (req, res, next) => {
       throw new ApiError('Passwords do not match', 400);
     }
 
+    if (!Object.keys(body).includes('name', 'username', 'email', 'password')) {
+      throw new ApiError(
+          'Payload must contain name, username, email and password',
+          400,
+      );
+    }
+
     const user = await User.create({
       username: body.username,
       email: body.email,
@@ -30,13 +37,78 @@ const getUserById = async (req, res, next) => {
 
     const user = await User.findOne({ where: { id: params.id } });
 
+    if (user === undefined || user.active === false) {
+      throw new ApiError('User not found', 400);
+    }
+
     res.json(new UserSerializer(user));
   } catch (err) {
     next(err);
   }
 };
 
+const updateUser = async (req, res, next) => {
+  try {
+    const { params } = req;
+    const { body } = req;
+    const user = await User.findOne({ where: { id: params.id } });
+
+    const data = Object.keys(body);
+    const toUpdate = ['name', 'username', 'email'];
+
+    const isValid = data.map((item) => {
+        let isValid = true;
+
+        if (!toUpdate.includes(item)) {
+            isValid = false;
+        }
+        
+        return isValid;
+    });
+
+    if (!isValid) {
+        throw new ApiError(
+            'Payload can only contain username, email or name',
+            400,
+        );
+    }
+
+    if (user === undefined || user.active === false) {
+        throw new ApiError('User not found', 400);
+    }
+
+    const userUpdated = await User.update({ where: { id: params.id } }, body);
+    res.json(new UserSerializer(userUpdated));
+  } catch (err) {
+      next(err);
+  }
+};
+
+const deleteUser = async (req, res, next) => {
+  try {
+      const { params } = req;
+      const user = await User.findOne({ where: { id: params.id } });
+
+      if (user === undefined || user.active === false) {
+          throw new ApiError('User not found', 400);
+      }
+
+      const data = {
+          status: 'success',
+          data: null,
+      };
+
+      user.active = false;
+      User.update({ where: { id: params.id } }, user);
+      res.json(data);
+  } catch (err) {
+      next(err);
+  }
+};
+
 module.exports = {
   createUser,
   getUserById,
+  updateUser,
+  deleteUser
 };
