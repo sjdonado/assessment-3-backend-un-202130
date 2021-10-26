f0bae90143646136d3f409fc6b6b81590095d6cb
Ronaldo Carreño Miranda <ronaldoc@uninorte.edu.co>
2021-10-26 02:55:57 -0500

    feat updateUser + deleteUser
    commit para testear los cambios

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,4 +8,8 @@ router.get('/:id', UsersController.getUserById);
 
 router.post('/', UsersController.createUser);
 
+router.post('/', UsersController.updateUser);
+
+router.post('/', UsersController.deleteUser);
+
 module.exports = router;
