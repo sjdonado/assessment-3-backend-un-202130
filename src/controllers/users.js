cadaceaef63b7db4e1d68a70b4d287f5cf8ac430
Andres <50928089+Andresifico@users.noreply.github.com>
2021-11-05 11:58:27 -0500

    fix(users): fix linter

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,29 +1,24 @@
 const ApiError = require('../utils/ApiError');
 const User = require('../models/user');
+
 const UserSerializer = require('../serializers/UserSerializer');
+
 const createUser = async (req, res, next) => {
   try {
     const { body } = req;
 
-    if (body.name === undefined) 
-    {
-        throw new ApiError('Payload must contain name, username, email and password', 400);
+    if (body.name === undefined) {
+      throw new ApiError('Payload must contain name, username, email and password', 400);
     }
-    
-    if (body.username === undefined) 
-    {
-        throw new ApiError('Payload must contain name, username, email and password', 400);
+    if (body.username === undefined) {
+      throw new ApiError('Payload must contain name, username, email and password', 400);
     }
-    
-    if (body.email === undefined) 
-    {
-        throw new ApiError('Payload must contain name, username, email and password', 400);
+    if (body.email === undefined) {
+      throw new ApiError('Payload must contain name, username, email and password', 400);
     }
-    
     if (body.password !== body.passwordConfirmation) {
       throw new ApiError('Passwords do not match', 400);
     }
-
     const user = await User.create({
       username: body.username,
       email: body.email,
@@ -59,11 +54,11 @@ const updateUser = async (req, res, next) => {
     const { params } = req;
     const { body } = req;
     const user = await User.findOne({ where: { id: params.id } });
-    
+
     if (!user || user.active === false) {
       throw new ApiError('User not found', 400);
     }
-    
+
     if ((body.username === undefined && body.email === undefined
       && body.name === undefined) || body.password !== undefined) {
       throw new ApiError('Payload can only contain username, email or name', 400);
@@ -80,11 +75,10 @@ const getUserById = async (req, res, next) => {
     const { params } = req;
 
     const user = await User.findOne({ where: { id: params.id } });
-    
-   if (user === undefined || user.active === false) 
-   {
+
+    if (user === undefined || user.active === false) {
       throw new ApiError('User not found', 400);
-   }
+    }
 
     res.json(new UserSerializer(user));
   } catch (err) {
